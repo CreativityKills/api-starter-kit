7268fde896eec4199f32cdcd3c66f9d756b31bce
Neo <public@neoi.sh>
2025-08-30 17:19:20 +0200

    Initial commit.

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Models\User;
 use Symfony\Component\HttpFoundation\Response;
 
-if (!function_exists('currentUserOrFail')) {
+if (! function_exists('currentUserOrFail')) {
     function currentUserOrFail(int $code = Response::HTTP_NOT_FOUND): User
     {
         return request()->user() ?? abort($code);
